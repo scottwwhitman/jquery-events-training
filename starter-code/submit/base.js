a53c164fbd39fb67f28d2f6939fbe7655b219c43
Scott Whitman <scott.w.whitman@gmail.com>
2016-08-18 16:18:18 -0700

    adding both ready and submit

--- starter-code/submit/base.js
+++ starter-code/submit/base.js
@@ -6,4 +6,9 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
+  $("form").on("click", function handleClick(event){
+    event.preventDefault();
+    var $newForm = $('<form action="https://www.youtube.com/watch" method="GET"><input name="v" value="dQw4w9WgXcQ" hidden><input type="submit"></form>');
+    $('body').append($newForm);
+  });
 })
