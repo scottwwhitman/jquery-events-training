d50c3ae48d739c1820fd2d3c79b13ac9bd7aaf95
Scott Whitman <scott.w.whitman@gmail.com>
2016-08-18 22:33:13 -0700

    adding click

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -2,4 +2,12 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  $("span").on("click", function print(event) {
+    var span = $(this);
+    span.addClass('span-active');
+    var spanMake = $("<li>You Clicked: <span>" + $(".span-active").text() + "</span> at <span>" + Date.now() + "</li>");
+    $("ul").append(spanMake);
+    span.removeClass('span-active');
+  });
+
 })
