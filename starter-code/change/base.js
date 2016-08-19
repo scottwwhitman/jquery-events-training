ec5e8a1618714931278597090160f244af1ccb62
Scott Whitman <scott.w.whitman@gmail.com>
2016-08-18 21:35:13 -0700

    adding change

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
+  $(".add").on("change", function calculate(event) {
+    var left = +$('#left').val();
+    var right = +$('#right').val();
+    var sum = left + right;
+    $('#total').val(sum);
+  });
+  var r= $('<input type="button" value="Clear board"/>');
+  $("body").append(r);
+  $('button').on('click', function reset(event) {
+    $('#left')[0].reset();
+    $('#right')[0].reset();
+    $('#total')[0].reset();
+  })
 })
